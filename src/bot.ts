22a8f7ac515f84b636b5510fac5ffea78ae54b1f
DaniDipp <dippigames@gmail.com>
2021-10-24 17:36:23 +0200

    initial commit

---
+++ src/bot.ts
@@ -0,0 +1,21 @@
+import { Client, Intents } from "discord.js";
+import { config } from "dotenv";
+config();
+
+const client = new Client({
+	intents: [
+		Intents.FLAGS.GUILDS,
+		//Intents.FLAGS.GUILD_MEMBERS,
+		Intents.FLAGS.GUILD_MESSAGES,
+		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+		Intents.FLAGS.GUILD_MESSAGE_TYPING,
+	],
+});
+
+client.once("ready", () => {
+	console.log("Ready!");
+});
+
+const token = process.env.DISCORD_TOKEN;
+if (!token) throw new Error("No token found!");
+client.login(token);
