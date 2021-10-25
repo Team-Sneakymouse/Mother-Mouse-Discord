4228012e900a0a1819eb75071569661a86195ffd
DaniDipp <dippigames@gmail.com>
2021-10-25 06:32:59 +0200

    Clean up new member roles

--- src/NewMemberRoles.ts
+++ src/NewMemberRoles.ts
@@ -1,30 +1,41 @@
 import { Client } from "discord.js";
+
+enum GuildIds {
+	RAWBTV = "391355330241757205",
+	TILII = "768372809616850964",
+	SNEAKYRP = "725854554939457657",
+}
+
+enum RoleIds {
+	RATS = "631608275883917332",
+	COMMUNITY_MEMBER = "823624104900689961",
+}
+
 export default function NewMemberRoles(client: Client) {
 	client.on("guildMemberAdd", async (member): Promise<any> => {
 		await new Promise((resolve) => setTimeout(resolve, 500));
 		await member.fetch(true);
+
 		console.log(
-			`${member.user.username}#${member.user.discriminator} has joined ${member.guild.name} with roles: ${member.roles.cache
-				.map((r) => r.name)
-				.join(",")}`
+			`${member.user.username}#${member.user.discriminator} has joined ${member.guild.name}`,
+			member.roles.cache.map((r) => r.name)
 		);
 
-		// rawb.tv
-		if (member.guild.id === "391355330241757205") {
+		if (member.guild.id === GuildIds.RAWBTV) {
 			if (member.roles.highest.name === "@everyone") {
-				return await member.roles.add("631608275883917332"); // Rats
+				console.log("Adding RATS role");
+				return await member.roles.add(RoleIds.RATS);
 			}
 		}
 
-		// TILII
-		if (member.guild.id === "768372809616850964") {
+		if (member.guild.id === GuildIds.TILII) {
 			if (member.roles.highest.name === "@everyone") {
-				return await member.roles.add("823624104900689961"); // Community Member
+				console.log("Adding Community Member role");
+				return await member.roles.add(RoleIds.COMMUNITY_MEMBER);
 			}
 		}
 
-		// SneakyRP
-		//if (member.guild.id === "725854554939457657") {
+		//if (member.guild.id === GuildIds.SNEAKYRP) {
 		//}
 	});
 }
