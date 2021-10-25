9d1713186cbc29ea4400004debce929e087373af
DaniDipp <dippigames@gmail.com>
2021-10-25 21:31:21 +0200

    clean up command registration

--- registerCommands.ts
+++ registerCommands.ts
@@ -4,6 +4,8 @@ import { ApplicationCommand } from "discord.js/typings/index.js";
 import { config } from "dotenv";
 config();
 
+const clientId = "713723936231129089";
+
 enum GuildIds {
 	TEST = "155020885521203200",
 	RAWBTV = "391355330241757205",
@@ -11,11 +13,16 @@ enum GuildIds {
 	SNEAKYRP = "725854554939457657",
 }
 
-import { data as Vibecheck } from "./src/Vibecheck";
-const commands = [Vibecheck.toJSON()];
+const commands = {
+	global: [],
+	[GuildIds.TEST]: [],
+	[GuildIds.RAWBTV]: [],
+	[GuildIds.TILII]: [],
+	[GuildIds.SNEAKYRP]: [],
+};
 
-const clientId = "713723936231129089";
-const guildId = null; //GuildIds.TEST; //null
+import { data as Vibecheck } from "./src/Vibecheck";
+commands["global"].push(Vibecheck);
 
 const rest = new REST({ version: "9" }).setToken(process.env.DISCORD_TOKEN!);
 
@@ -23,8 +30,14 @@ const rest = new REST({ version: "9" }).setToken(process.env.DISCORD_TOKEN!);
 	try {
 		console.log("Started refreshing application (/) commands.");
 
-		if (guildId) await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands });
-		else await rest.put(Routes.applicationCommands(clientId), { body: commands });
+		for (const [guildId, guildCommands] of Object.entries(commands)) {
+			console.log(`Refreshing application commands for ${guildId}`);
+			if (guildId === "global") {
+				await rest.put(Routes.applicationCommands(clientId), { body: guildCommands });
+			} else {
+				await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: guildCommands });
+			}
+		}
 
 		console.log("Successfully reloaded application (/) commands.");
 	} catch (error) {
