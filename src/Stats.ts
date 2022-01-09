adf76c26fa46d7b6a708ef027ee60a7138db1615
DaniDipp <dippigames@gmail.com>
2022-01-09 21:04:13 +0100

    Add counter for every message snet

--- src/Stats.ts
+++ src/Stats.ts
@@ -4,8 +4,10 @@ import { Redis } from "ioredis";
 export default function MediaEmbeds(client: Client, redis: Redis) {
 	client.on("messageCreate", async (message) => {
 		if (message.guildId !== "391355330241757205") return;
-		if (!message.content.match(/kobold/i)) return;
+		redis.zincrby("mm-discord-stats:messages", 1, message.author.id);
 
-		await redis.zincrby("mm-discord-stats:kobold", 1, message.author.id);
+		if (message.content.match(/kobold/i)) {
+			redis.zincrby("mm-discord-stats:kobold", 1, message.author.id);
+		}
 	});
 }
