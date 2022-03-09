1a2a85ec375e3fbb630bd0486bfa81b14628add3
DaniDipp <dippigames@gmail.com>
2022-03-09 18:22:00 +0100

    Auto-unarchive threads of unresolved issues

--- src/Gitlab/index.ts
+++ src/Gitlab/index.ts
@@ -7,6 +7,7 @@ import initCommands from "./commands";
 import initModals from "./modals";
 import initButtons from "./buttons";
 import initMessages from "./messages";
+import initThreads from "./threads";
 
 export { data } from "./commands";
 export default function Gitlab(client: Client, redis: Redis, server: Express) {
@@ -18,6 +19,7 @@ export default function Gitlab(client: Client, redis: Redis, server: Express) {
 	const modals = initModals(client, redis, gitlab);
 	const buttons = initButtons(client, gitlab);
 	const messages = initMessages(client, gitlab);
+	const threads = initThreads(client, gitlab);
 
 	client.on("interactionCreate", (interaction) => {
 		if (commands.matcher(interaction)) return commands.handler(interaction);
@@ -25,7 +27,10 @@ export default function Gitlab(client: Client, redis: Redis, server: Express) {
 		if (buttons.matcher(interaction)) return buttons.handler(interaction);
 	});
 	client.on("messageCreate", (m) => {
-		if (messages.matcher(m)) messages.handler(m);
+		if (messages.matcher(m)) return messages.handler(m);
+	});
+	client.on("threadUpdate", (_, t) => {
+		if (threads.matcher(t)) return threads.handler(t);
 	});
 
 	const webhookHandler = initWebhook(client, redis, gitlab);
---
+++ src/Gitlab/threads.ts
@@ -0,0 +1,19 @@
+import { Gitlab } from "@gitbeaker/node";
+import { ChannelType, Client, ThreadChannel } from "discord.js";
+import { projectIds, Projects } from "./utils";
+
+export default function (client: Client, gitlab: InstanceType<typeof Gitlab>) {
+	return {
+		matcher: function (t: ThreadChannel) {
+			return t.type === ChannelType.GuildPublicThread && t.ownerId === client.user?.id && t.archived === true;
+		},
+		handler: async function (t: ThreadChannel) {
+			const starterMessage = await t.fetchStarterMessage();
+			const projectId = projectIds[t.guildId as Projects];
+			const issueId = parseInt(starterMessage.embeds[0].footer?.text.match(/\d+/)?.[0] as string);
+			if (!projectId || !issueId) return;
+			const issue = await gitlab.Issues.show(projectId, issueId);
+			if (issue.state !== "closed") await t.setArchived(false);
+		},
+	};
+}
