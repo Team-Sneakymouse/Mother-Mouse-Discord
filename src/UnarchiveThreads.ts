c5c89e4bcb50d3eb4d6360a947def7288be042d0
DaniDipp <dippigames@gmail.com>
2022-04-18 15:52:35 +0200

    Restrict Gitlab Thread archive checking

--- src/UnarchiveThreads.ts
+++ src/UnarchiveThreads.ts
@@ -1,6 +1,6 @@
 import { ChannelType, Client } from "discord.js";
 import { Gitlab } from "@gitbeaker/node";
-import { projectIds, Projects } from "./Gitlab/utils";
+import { channelIds, projectIds, Projects } from "./Gitlab/utils";
 
 export default function UnarchiveThreads(client: Client, gitlab: InstanceType<typeof Gitlab>) {
 	client.on("threadUpdate", async (_, thread) => {
@@ -11,17 +11,19 @@ export default function UnarchiveThreads(client: Client, gitlab: InstanceType<ty
 		const starterMessage = await thread.fetchStarterMessage();
 
 		// Gitlab
-		const projectId = projectIds[thread.guildId as Projects];
-		const issueId = parseInt(starterMessage.embeds[0].footer?.text.match(/\d+/)?.[0] as string);
-		if (!projectId || !issueId) {
-			try {
-				const issue = await gitlab.Issues.show(projectId, issueId);
-				if (issue) {
-					if (issue.state !== "closed") await thread.setArchived(false);
-					return;
+		if (Object.keys(channelIds).includes(starterMessage.channelId)) {
+			const projectId = projectIds[thread.guildId as Projects];
+			const issueId = parseInt(starterMessage.embeds[0].footer?.text.match(/\d+/)?.[0] as string);
+			if (!projectId || !issueId) {
+				try {
+					const issue = await gitlab.Issues.show(projectId, issueId);
+					if (issue) {
+						if (issue.state !== "closed") await thread.setArchived(false);
+						return;
+					}
+				} catch (e) {
+					console.error(`Error in fetching gitlab issue ${projectId}, ${issueId}. Is it just missing?`, e);
 				}
-			} catch (e) {
-				console.error(`Error in fetching gitlab issue ${projectId}, ${issueId}. Is it just missing?`, e);
 			}
 		}
 
