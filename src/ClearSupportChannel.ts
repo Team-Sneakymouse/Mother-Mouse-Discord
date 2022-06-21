fbdd9c9bac34f53c4fa4676b750d0285c02113dd
mami <mamoniot@protonmail.com>
2022-06-21 18:37:20 -0400

    added onetime event

--- src/ClearSupportChannel.ts
+++ src/ClearSupportChannel.ts
@@ -185,4 +185,6 @@ export default function ClearSupportChannel(client: Client, redis: Redis) {
 			EndVote(redis, client, channelId, scheduledTime);
 		});
 	}
+
+	ScheduleOnce(redis, eventEndVotingKey + "975496209882050640", Math.floor(Date.now() / 1000) + 60);
 }
