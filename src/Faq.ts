23f0d19953361ec54f33a0f135708f8da9b7c595
DaniDipp <dippigames@gmail.com>
2023-04-24 23:18:01 +0000

    cache faq questions for 5 secs

--- src/Faq.ts
+++ src/Faq.ts
@@ -13,6 +13,9 @@ export const data = [
 		),
 ];
 
+type FaqQuestion = { question: string; answer: string; tags: string };
+const questionCache: Record<string, (PBRecord & FaqQuestion)[] | null> = {};
+
 export default function Vibecheck(client: Client, pocketbase: PocketBase) {
 	client.on("interactionCreate", async (interaction) => {
 		if (interaction.isAutocomplete() && interaction.commandName === "faq") {
@@ -26,11 +29,14 @@ export default function Vibecheck(client: Client, pocketbase: PocketBase) {
 
 			const autocomplete = interaction.options.getFocused().toLowerCase();
 
-			const questions = await pocketbase
-				.collection("mm_faq")
-				.getFullList<PBRecord & { question: string; answer: string; tags: string }>(undefined, {
+			let questions = questionCache[interaction.guild.id];
+			if (!questions) {
+				questions = await pocketbase.collection("mm_faq").getFullList<PBRecord & FaqQuestion>(undefined, {
 					filter: `server="${interaction.guild.id}"`,
 				});
+				questionCache[interaction.guild.id] = questions;
+				setTimeout(() => (questionCache[interaction.guild!.id] = null), 5000);
+			}
 			const relevantQuestions = questions.filter(
 				(question) =>
 					question.question.toLowerCase().includes(autocomplete) ||
@@ -45,9 +51,7 @@ export default function Vibecheck(client: Client, pocketbase: PocketBase) {
 		if (interaction.isChatInputCommand() && interaction.commandName === "faq") {
 			const questionId = interaction.options.getString("query", true);
 			try {
-				const question = await pocketbase
-					.collection("mm_faq")
-					.getOne<PBRecord & { question: string; answer: string; tags: string }>(questionId);
+				const question = await pocketbase.collection("mm_faq").getOne<PBRecord & FaqQuestion>(questionId);
 
 				interaction.reply({
 					content: "",
