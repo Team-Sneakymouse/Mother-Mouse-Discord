9e9f98dffbc7f5032416ac116040fc2b054a4f4e
DaniDipp <dippigames@gmail.com>
2023-07-09 22:31:41 +0000

    exempt server owner from channel filter

--- src/ChannelFilters.ts
+++ src/ChannelFilters.ts
@@ -2,6 +2,8 @@ import { Client } from "discord.js";
 
 export default function ChannelFilters(client: Client) {
 	client.on("messageCreate", (message) => {
+		if (message.author.id === message.guild?.ownerId) return; // Server owner exempt
+
 		if (message.guildId === "391355330241757205") {
 			// rawb.tv
 			if (message.channelId === "1096954434946347149") {
