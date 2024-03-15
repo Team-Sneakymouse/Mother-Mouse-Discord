60ad4998363b2ff888f8b0c61f9bb3c729bf0715
DaniDipp <dippigames@gmail.com>
2024-03-15 00:16:33 +0000

    allow text command param substitution

--- src/TextCommands.ts
+++ src/TextCommands.ts
@@ -1,4 +1,4 @@
-import { Client } from "discord.js";
+import { Client, Message } from "discord.js";
 import PocketBase, { Record as PBRecord } from "pocketbase";
 
 type CommandRecord = { server: string; command: string; response: string };
@@ -9,7 +9,7 @@ export default function TextCommands(client: Client, pocketbase: PocketBase) {
 		if (!message.content.startsWith("!")) return;
 
 		const args = message.content.split(" ");
-		const command = args.shift()!.toLowerCase();
+		const command = args[0].toLowerCase();
 
 		const record = await pocketbase
 			.collection("commands")
@@ -20,9 +20,34 @@ export default function TextCommands(client: Client, pocketbase: PocketBase) {
 			});
 		if (!record) return;
 
+		const response = parseResponse(args, record.response, message);
+
 		message.reply({
-			content: record?.response,
+			content: response,
 			allowedMentions: { repliedUser: false },
 		});
 	});
+
+	function parseResponse(args: string[], response: string, message: Message) {
+		return response
+			.replaceAll("${user}", message.author.displayName)
+			.replaceAll("${channel}", `<#${message.channel.id}>`)
+			.replace(/\${random\.(\d+)-(\d+)}/g, (match, ...captureGroups) => {
+				const start = parseInt(captureGroups[0]);
+				const end = parseInt(captureGroups[1]);
+				return (Math.floor(Math.random() * (end - start + 1)) + start).toString();
+			})
+			.replace(/\${(\d+)}/g, (match, ...captureGroups) => args[parseInt(captureGroups[0])])
+			.replace(/\${(\d+):(\d+)?}/g, (match, ...captureGroups) => {
+				const start = Math.min(parseInt(captureGroups[0]), args.length - 1);
+				const end = typeof captureGroups[1] === "string" ? parseInt(captureGroups[1]) : args.length - 1;
+				return args.slice(start, Math.max(start, end) + 1).join(" ");
+			})
+			.replace(/\${random\.pick ([^}]+)}/g, (match, ...captureGroups) => {
+				const choices = captureGroups[0].split("' '");
+				choices[0] = choices[0].replace(/^'/, "");
+				choices[choices.length - 1] = choices[choices.length - 1].replace(/'$/, "");
+				return choices[Math.floor(Math.random() * choices.length)];
+			});
+	}
 }
