aaa06b1f6c0c13fa99b1e09e858c24ebbf4256b1
DaniDipp <dippigames@gmail.com>
2026-03-06 00:37:15 +0000

    blockbench images

--- src/BlockbenchDownloader.ts
+++ src/BlockbenchDownloader.ts
@@ -1,4 +1,4 @@
-import { Client, AttachmentBuilder, MessageFlags } from "discord.js";
+import { Client, AttachmentBuilder, MessageFlags, MediaGalleryBuilder, MediaGalleryItemBuilder, FileBuilder } from "discord.js";
 
 export default function BlockbenchDownloader(client: Client) {
 	client.on("messageCreate", async (message) => {
@@ -12,23 +12,37 @@ export default function BlockbenchDownloader(client: Client) {
 			message.channel.sendTyping();
 
 			const codes = links.map((link) => link.replace(/\/$/, "").split("/").pop());
-			const attachments = [];
+			const attachments: AttachmentBuilder[] = [];
 			for (const code of codes) {
 				let json;
 				try {
-					json = await fetch(`https://www.blckbn.ch/api/models/${code}`).then((res) => res.json());
+					json = await fetch(`https://blckbn.ch/api/models/${code}`).then((res) => res.json());
 				} catch (e) {
 					console.error("Failed to fetch blockbench model", code);
 					console.error(e);
 					continue;
 				}
 				const name = json.name || code;
-				attachments.push(new AttachmentBuilder(`https://www.blckbn.ch/api/models/${code}`).setName(`${name}.bbmodel`));
+				attachments.push(
+					new AttachmentBuilder(`https://blckbn.ch/api/models/${code}`).setName(`${name}.bbmodel`),
+					new AttachmentBuilder(`https://blckbn.ch/thumb/${code}.png`).setName(`${name}.png`),
+				);
 			}
 			if (attachments.length === 0) return;
 
 			await message.channel.send({
-				content: "\u00A0",
+				flags: MessageFlags.IsComponentsV2,
+				components: [
+					new MediaGalleryBuilder().addItems(...attachments
+						.filter(a => a.name?.endsWith(".png"))
+						.map(a => new MediaGalleryItemBuilder()
+							.setURL(`attachment://${a.name}`)
+							.setDescription(a.name || "unknown.png")
+						)
+					),
+					...attachments.filter(a => a.name?.endsWith(".bbmodel"))
+						.map(a => new FileBuilder().setURL(`attachment://${a.name}`))
+				],
 				files: attachments,
 				reply: { messageReference: message },
 				allowedMentions: { repliedUser: false },
